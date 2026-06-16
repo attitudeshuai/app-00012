0f77e06b50bda4872c14d67fcfc467e761aab55c
attitudeshuai <848001189@qq.com>
2026-06-17 02:25:07 +0800

    upload src/main/java/com/toolshare/entity/NotificationType.java

---
+++ src/main/java/com/toolshare/entity/NotificationType.java
@@ -0,0 +1,10 @@
+package com.toolshare.entity;
+
+public enum NotificationType {
+    BORROW_APPROVED,
+    BORROW_REJECTED,
+    BORROW_RETURNED,
+    BORROW_OVERDUE,
+    BORROW_DUE_SOON,
+    NEW_BORROW_REQUEST
+}
